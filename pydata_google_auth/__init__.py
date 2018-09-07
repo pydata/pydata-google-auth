72a17195a524085396c1107eb944814395f74405
Tim Swast <swast@google.com>
2018-09-07 13:42:44 -0700

    Update docstring.

--- pydata_google_auth/__init__.py
+++ pydata_google_auth/__init__.py
@@ -7,15 +7,9 @@ versions = get_versions()
 __version__ = versions.get('closest-tag', versions['version'])
 __git_revision__ = versions['full-revisionid']
 
-"""Google BigQuery API wrapper.
-The main concepts with this API are:
-- :class:`~google.cloud.bigquery.client.Client` manages connections to the
-  BigQuery API. Use the client methods to run jobs (such as a
-  :class:`~google.cloud.bigquery.job.QueryJob` via
-  :meth:`~google.cloud.bigquery.client.Client.query`) and manage resources.
-- :class:`~google.cloud.bigquery.dataset.Dataset` represents a
-  collection of tables.
-- :class:`~google.cloud.bigquery.table.Table` represents a single "relation".
+"""pydata-google-auth
+
+This package provides helpers for fetching Google API credentials.
 """
 
 __all__ = [
